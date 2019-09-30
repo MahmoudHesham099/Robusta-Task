c7043af205974f03c5fe052176dc9660ba4f9902
hesham <mahmoudhesham297199@gmail.com>
2019-09-30 22:33:38 +0200

    initial

---
+++ script.js
@@ -0,0 +1,48 @@
+
+var color = document.getElementById("colorPicker");
+
+var height = document.getElementById("inputHeight");
+var width = document.getElementById("inputWidth");
+
+var table = document.getElementById("pixelCanvas");
+
+
+// var rows=height.value;
+// var columns=width.value;
+
+// height.addEventListener('change',function(){
+//      rows =height.value;
+// })
+
+// width.addEventListener('change',function(){
+//     columns =width.value;
+// })
+
+
+function makeGrid(){
+ 
+    var newRows =height.value;
+    var newColumns =width.value;
+   
+    var oldRows = table.rows.length 
+    
+    // clear the old table
+    // table.innerHTML=""
+    // OR by loop
+    for (let i = 0; i < oldRows; i++) {
+        var oldColumns = table.rows[i].cells.length    
+        for (let y = 0; y < oldColumns; y++) { 
+             table.rows[i].deleteCell(table.rows[i].cells.length-1)  
+    }}
+
+    //create the new table
+    for (let i = 0; i < newRows; i++) {
+        var row = table.insertRow(i);
+        for (let y = 0; y < newColumns; y++) {
+          var cell=row.insertCell(y);
+          cell.addEventListener('click',function(){
+            var newColor=color.value
+            table.rows[i].cells[y].style.backgroundColor = newColor;
+          })
+    }}
+}
